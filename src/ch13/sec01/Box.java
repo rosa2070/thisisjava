0e259635c0a2309d431bb3d14e1457a7b70aaaae
jaeeun <rosa2070@naver.com>
2024-02-02 22:31:16 +0900

    02/02

--- src/ch13/sec01/Box.java
+++ src/ch13/sec01/Box.java
@@ -2,5 +2,4 @@ package ch13.sec01;
 
 public class Box<T> {
     public T content;
-    
 }
--- src/ch13/sec01/GenericExample.java
+++ src/ch13/sec01/GenericExample.java
@@ -8,5 +8,11 @@ public class GenericExample {
         String str = box1.content;
         System.out.println(str);
 
+//        Box<Integer> box2 = new Box<Integer>();
+        Box<Integer> box2 = new Box<>();
+        box2.content = 100;
+        int value = box2.content;
+        System.out.println(value);
+
     }
 }
