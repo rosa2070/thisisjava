e4e572c57eba361c47fc71f3047cd161abb14a00
jaeeun <rosa2070@naver.com>
2024-02-13 23:54:09 +0900

    02/13

---
+++ src/quiz/quiz16to17/Printer.java
@@ -0,0 +1,24 @@
+package quiz.quiz16to17;
+
+public class Printer {
+    public static void println(int value) {
+        System.out.println(value);
+    }
+
+    public static void println(boolean value) {
+        System.out.println(value);
+
+    }
+
+    public static void println(double value) {
+        System.out.println(value);
+
+    }
+
+    public static void println(String value) {
+        System.out.println(value);
+
+    }
+
+
+}
---
+++ src/quiz/quiz19/Account.java
@@ -0,0 +1,21 @@
+package quiz.quiz19;
+
+public class Account {
+    public static final int MIN_BALANCE = 0;
+    public static final int MAX_BALANCE = 1000000;
+    private int balance;
+
+    public void setBalance(int balance) {
+        if (balance < MIN_BALANCE || balance > MAX_BALANCE) {
+            return;
+        } else {
+            this.balance = balance;
+        }
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+
+}
